a504343a89dfb629ad13cd7d895e18f06b2df089
Dylan Seidt <dylan.seidt@gmail.com>
2018-10-14 11:22:44 -0500

    Add mass message command

--- index.js
+++ index.js
@@ -7,8 +7,9 @@ const request = require('request');
 var Redis = require('ioredis');
 var redis = new Redis(process.env.REDIS_URL);
 
-redis.hsetnx('commands', 'channel', fs.readFileSync(__dirname + '/commands/channel.txt'));
 redis.hsetnx('commands', 'help', fs.readFileSync(__dirname + '/commands/help.txt'));
+redis.hsetnx('commands', 'channel', fs.readFileSync(__dirname + '/commands/channel.txt'));
+redis.hsetnx('commands', 'message', fs.readFileSync(__dirname + '/commands/message.txt'));
 
 client.on('ready', () => {
 	client.user.setPresence({
@@ -22,17 +23,28 @@ client.on('ready', () => {
 
 client.on('message', msg => {
 	if (msg.content.startsWith((process.env.PREFIX || 's!') + ' ')) {
-		var content = msg.content.split(' ');
-		var invoke = content[1];
-		var args = content.slice(2);
-		
-		redis.hgetall('commands', function (err, result) {
-			for (var index in result) {				
-				if (index == invoke) {
-					eval('try {' + result[index] + '} catch(e) { console.log(e); msg.reply("an error occurred when executing that command. Check the console for more information."); }');
-				}
-			}
-		});
+        if (msg.guild) {
+            var content = msg.content.split(' ');
+            var invoke = content[1];
+            var args = content.slice(2);
+            
+            redis.hgetall('commands', function (err, result) {
+                var found = false;
+                
+                for (var index in result) {				
+                    if (index == invoke) {
+                        found = true;
+                        eval('try {' + result[index] + '} catch(e) { console.log(e); msg.reply("an error occurred when executing that command. Check the console for more information."); }');
+                    }
+                }
+                
+                if (!found) {
+                    msg.reply('that command does not exist. Refer to command help by running: `'+ (process.env.PREFIX || 's!') + ' help`');
+                }
+            });
+        } else {
+            msg.author.send('Commands can only be run in a channel on the discord server.');
+        }
 	}
 });
 
